3dc651464cd7fc9f1427572d8879ed664fbf2072
Bartłomiej <100492932+KokoszaKRA@users.noreply.github.com>
2022-09-30 23:13:19 +0200

    Revert "Add scripts"

--- js/task-02.js
+++ js/task-02.js
@@ -6,16 +6,3 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
-const list = document.querySelector("ul#ingredients");
-
-const listItem = ingredients.map(el => {
-  const newItem = document.createElement("li");
-  newItem.textContent = el ;
-  newItem.classList.add("item");
-return newItem;
-});
-
-list.append(...listItem);
-
-
