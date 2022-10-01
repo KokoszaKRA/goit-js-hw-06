0528956d0d5c2f62084ac320b7eb18d24a83ca84
Bartlomiej Kokosza <bartlomiej.kokosza@gmail.com>
2022-10-01 22:58:08 +0200

    remove empty line

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,6 @@ input.addEventListener("blur" , (ev) => {
     if (ev.currentTarget.value.length <= ev.currentTarget.getAttribute('data-length')) 
     ev.currentTarget.setAttribute('class', 'valid')
     else ev.currentTarget.setAttribute('class', 'invalid');
-    
    
     console.log(ev.currentTarget.value.length);
     console.log(ev.currentTarget.getAttribute('data-length'));
